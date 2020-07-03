c2dfec6a0ade9b02b3acdf89be118716f6d9da3e
skmbw <tongku2008@126.com>
2020-07-03 10:20:49 +0800

    对空指针的一些练习

---
+++ ch02/ex2_pointer.cpp
@@ -0,0 +1,36 @@
+//
+// Created by yinlei on 2020/7/3.
+//
+
+#include <iostream>
+using namespace std;
+
+/**
+ * 指针相关的练习。
+ *
+ * 指针是一个对象。
+ * 指针指向其他对象的地址。
+ * 指针的类型要和它所指向对象的类型匹配。
+ * 指针在其声明周期内，可以指向不同的对象。
+ *
+ * @return
+ */
+int main() {
+    int i = 1;
+    double d = 1.2;
+    // C++代码格式自由，*和变量名之间可以有空格。
+    int *intPointer = &i;
+
+    // 这三种都是定义空指针的，推荐第一种
+    double *nullPointer1 = nullptr;
+    double *nullPointer2 = NULL;
+    double *nullPointer3 = 0;
+
+    // intPointer是一个指针，要使用它所指向的值，必须使用解引用符（*）来获得它所指向的对象
+    // intPointer = 2;
+    *intPointer = 2;
+
+    cout << i << " " << *intPointer << endl;
+
+    return 0;
+}
